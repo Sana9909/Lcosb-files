b75111e8c0531e74da91540e2dfa97ceee1d45e2
Sneha Lahiri <snehalahiri09@gmail.com>
2023-09-30 18:29:34 +0530

    added bias

--- RRT.py
+++ RRT.py
@@ -8,14 +8,27 @@ def main():
     goal=(510,510)
     obsdim=30
     obsnum=50
+    iteration=0
 
     pygame.init()
     map=RRTMap(start,goal,dimensions,obsdim,obsnum)
     graph=RRTGraph(start,goal,dimensions,obsdim,obsnum)
 
     obstacles=graph.makeobs()
+    map.drawMap(obstacles) 
 
-    map.drawMap(obstacles)
+    while(iteration<500):
+        if iteration%10==0:
+            X,Y,Parent=graph.bias(goal)
+            pygame.draw.circle(map.map,map.grey,(X[-1],Y[-1]),map.nodeRad+2,0)
+            pygame.draw.line(map.map,map.Blue,(X[-1],Y[-1]),(X[Parent[-1]],Y[Parent[-1]]),map.edgeThickness)
+        else:
+            X,Y,Parent=graph.expand()
+            pygame.draw.circle(map.map,map.grey,(X[-1],Y[-1]),map.nodeRad+2,0)
+            pygame.draw.line(map.map,map.Blue,(X[-1],Y[-1]),(X[Parent[-1]],Y[Parent[-1]]),map.edgeThickness)
+        if iteration%10==0:
+            pygame.display.update()
+        iteration+=1
 
     pygame.display.update()
     pygame.event.clear()
